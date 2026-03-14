9efa0442fc60c3b90516ed9d28f4e78c3174ec8e
Amir-Mallek <amir.mallek@insat.ucar.tn>
2026-03-15 00:00:20 +0100

    feat: add cli and dockerfiles

--- cmd/master/main.go
+++ cmd/master/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	mastercli "github.com/Amir-Mallek/Distributed-Dataset-Repository/cli/master"
+)
 
 func main() {
-	fmt.Println("hello master!")
+	if err := mastercli.NewRootCmd().Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
--- cmd/stserver/main.go
+++ cmd/stserver/main.go
@@ -1,31 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net"
 
-	pb "github.com/Amir-Mallek/Distributed-Dataset-Repository/api/chunktransfer"
-	chunktransfer "github.com/Amir-Mallek/Distributed-Dataset-Repository/internals/chunktransfer"
-	"google.golang.org/grpc"
-)
-
-const (
-	port = ":50051"
+	storagecli "github.com/Amir-Mallek/Distributed-Dataset-Repository/cli/storage"
 )
 
 func main() {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	grpcServer := grpc.NewServer()
-	server := chunktransfer.NewServer()
-	pb.RegisterChunkTransferServiceServer(grpcServer, server)
-
-	fmt.Printf("Storage server listening on %s\n", port)
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	if err := storagecli.NewRootCmd().Execute(); err != nil {
+		log.Fatal(err)
 	}
 }
